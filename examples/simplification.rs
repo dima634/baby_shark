23d31bec3ecda7cf9adb8b8438bc61be6926f2f9
hemosphere <h3mosphere@proton.me>
2023-08-30 17:17:21 +1000

    revert examples

--- examples/simplification.rs
+++ examples/simplification.rs
@@ -1,71 +1,20 @@
-use std::path::PathBuf;
+use std::path::Path;
 
 use baby_shark::{
-    decimation::{edge_decimation::ConstantErrorDecimationCriteria, prelude::EdgeDecimator},
-    io::stl::{StlReader, StlWriter}, mesh::{corner_table::prelude::CornerTableF, traits::Mesh}, rerun::{log_mesh, log_mesh_as_line_strips},
+    io::stl::{StlReader, StlWriter}, 
+    decimation::{prelude::EdgeDecimator, edge_decimation::ConstantErrorDecimationCriteria}, 
+    mesh::corner_table::prelude::CornerTableF
 };
-use rerun::{external::re_log, RecordingStream};
 
-
-#[derive(Debug, clap::Parser)]
-#[clap(author, version, about)]
-struct Args {
-    #[command(flatten)]
-    rerun: rerun::clap::RerunArgs,
-
-    #[clap(long, short, required = true)]
-    input_file: PathBuf,
-
-    #[clap(long, short, required = true)]
-    output_file: PathBuf,
-
-    #[clap(long, short, required = true)]
-    errors: Vec<f32>,
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    re_log::setup_native_logging();
-
-    use clap::Parser as _;
-    let args = Args::parse();
-
-    let default_enabled = true;
-    args.rerun
-        .clone()
-        .run("replay", default_enabled, move |session| {
-            run(&session, &args).unwrap();
-        })?;
-
-    Ok(())
-}
-
-fn run(rec_stream: &RecordingStream, args: &Args) -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let mut reader = StlReader::new();
-    let mut mesh: CornerTableF = reader
-        .read_stl_from_file(&args.input_file)
-        .expect("Read mesh from STL");
+    let mut mesh: CornerTableF = reader.read_stl_from_file(Path::new("./test_files/violin.stl")).expect("Read mesh from STL");
 
-    let _ = log_mesh_as_line_strips("original", None, &mesh, rec_stream);
-
-    println!("{:?}", args.errors);
-
-    for &error in &args.errors {
-        let decimation_criteria = ConstantErrorDecimationCriteria::new(error);
-
-        let mut decimator = EdgeDecimator::new().decimation_criteria(decimation_criteria);
-        let mut cloned = mesh.clone_remap();
-        decimator.decimate(&mut cloned);
-
-        let decimated = cloned.clone_remap();
-
-        let _ = log_mesh_as_line_strips(&format!("simplified-{error}"), None, &decimated, rec_stream);
-    }
+    let decimation_criteria = ConstantErrorDecimationCriteria::new(0.01f32);
+    
+    let mut decimator = EdgeDecimator::new().decimation_criteria(decimation_criteria);
+    decimator.decimate(&mut mesh);
 
     let writer = StlWriter::new();
-    writer
-        .write_stl_to_file(&mesh, &args.output_file)
-        .expect("Save mesh to STL");    
-    Ok(())
+    writer.write_stl_to_file(&mesh, Path::new("decimated.stl")).expect("Save mesh to STL");
 }
-
-
