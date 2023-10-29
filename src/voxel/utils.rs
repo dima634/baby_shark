ea28ff68a66cf572b2fc8c22e069c987f929948c
dima.volovyk <dima.volovyk@gmail.com>
2023-10-29 11:05:35 +0200

    log2 grid iter

--- src/voxel/utils.rs
+++ src/voxel/utils.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 use nalgebra::Vector3;
 
 /// Check if all bits in mask are zeroes
@@ -28,9 +30,58 @@ pub fn box_indices(start: isize, end: isize) -> impl Iterator<Item = Vector3<isi
     });
 }
 
-pub struct BoxIndices {
-    size: isize,
-    x: isize,
-    y: isize,
-    z: isize,
+pub struct GridIter {
+    log2: isize,
+    two_log2: isize,
+    range: Range<isize>,
+}
+
+impl GridIter {
+    pub fn from_origin_and_size(start: isize, log2: isize) -> Self {
+        let dim = 1 << log2;
+        let dim_squared = dim * dim;
+        let range = start..(dim_squared * dim);
+        let two_log2 = log2 + log2;
+
+        Self {
+            log2,
+            two_log2,
+            range,
+        }
+    }
+}
+
+impl Iterator for GridIter {
+    type Item = Vector3<isize>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next_flat = self.range.next()?;
+
+        let x = next_flat & self.log2;
+        let y = (next_flat >> self.log2) & self.log2;
+        let z = next_flat >> self.two_log2;
+
+        Some(Vector3::new(x, y, z))
+    }
+}
+
+mod tests {
+    use nalgebra::Vector3;
+
+    use super::GridIter;
+
+    #[test]
+    fn test_grid_iter() {
+        let mut it = GridIter::from_origin_and_size(0, 1);
+
+        assert_eq!(it.next(), Some(Vector3::new(0, 0, 0)));
+        assert_eq!(it.next(), Some(Vector3::new(1, 0, 0)));
+        assert_eq!(it.next(), Some(Vector3::new(0, 1, 0)));
+        assert_eq!(it.next(), Some(Vector3::new(1, 1, 0)));
+        assert_eq!(it.next(), Some(Vector3::new(0, 0, 1)));
+        assert_eq!(it.next(), Some(Vector3::new(1, 0, 1)));
+        assert_eq!(it.next(), Some(Vector3::new(0, 1, 1)));
+        assert_eq!(it.next(), Some(Vector3::new(1, 1, 1)));
+        assert_eq!(it.next(), None);
+    }
 }
