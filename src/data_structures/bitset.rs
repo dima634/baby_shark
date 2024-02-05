f6a5b6234e21e43dc37e1381a181f1eb108d1c22
dima.volovyk <dima.volovyk@gmail.com>
2024-02-05 18:57:43 +0200

    format

--- src/data_structures/bitset.rs
+++ src/data_structures/bitset.rs
@@ -1,4 +1,4 @@
-use std::ops::{BitAndAssign, BitOrAssign, BitXorAssign, BitXor, BitAnd, BitOr};
+use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign};
 
 #[derive(Debug, Clone, Copy)]
 pub struct BitSet<const BITS: usize, const STORAGE_SIZE: usize> {
@@ -20,7 +20,7 @@ impl<const BITS: usize, const STORAGE_SIZE: usize> BitSet<BITS, STORAGE_SIZE> {
     pub fn with(value: usize) -> Self {
         debug_assert!(BITS <= usize::BITS as usize * STORAGE_SIZE);
         debug_assert!(Self::unused_bits() < USIZE_BITS);
-    
+
         Self {
             storage: [value; STORAGE_SIZE],
         }
@@ -45,7 +45,7 @@ impl<const BITS: usize, const STORAGE_SIZE: usize> BitSet<BITS, STORAGE_SIZE> {
         }
 
         value |= self.storage[STORAGE_SIZE - 1] & Self::unused_mask();
-    
+
         value == 0
     }
 
@@ -71,7 +71,7 @@ impl<const BITS: usize, const STORAGE_SIZE: usize> BitSet<BITS, STORAGE_SIZE> {
 
         self.storage[storage_index] & value_mask != 0
     }
-    
+
     #[inline]
     pub fn set(&mut self, index: usize, value: bool) {
         debug_assert!(index < BITS);
@@ -85,22 +85,22 @@ impl<const BITS: usize, const STORAGE_SIZE: usize> BitSet<BITS, STORAGE_SIZE> {
             self.storage[storage_index] &= !value_mask;
         }
     }
-    
+
     #[inline]
     pub fn on(&mut self, index: usize) {
         self.set(index, true);
     }
-    
+
     #[inline]
     pub fn off(&mut self, index: usize) {
         self.set(index, false);
     }
-    
+
     #[inline]
     pub fn is_on(&mut self, index: usize) -> bool {
         self.at(index)
     }
-    
+
     #[inline]
     pub fn is_off(&mut self, index: usize) -> bool {
         !self.at(index)
@@ -141,15 +141,14 @@ impl<const BITS: usize, const STORAGE_SIZE: usize> BitAnd for BitSet<BITS, STORA
 
     #[inline]
     fn bitand(self, rhs: Self) -> Self::Output {
-        let mut storage: [usize; STORAGE_SIZE] = unsafe { std::mem::MaybeUninit::uninit().assume_init() };
+        let mut storage: [usize; STORAGE_SIZE] =
+            unsafe { std::mem::MaybeUninit::uninit().assume_init() };
 
         for i in 0..STORAGE_SIZE {
             storage[i] = self.storage[i] & rhs.storage[i];
         }
 
-        Self {
-            storage
-        }
+        Self { storage }
     }
 }
 
@@ -167,15 +166,14 @@ impl<const BITS: usize, const STORAGE_SIZE: usize> BitOr for BitSet<BITS, STORAG
 
     #[inline]
     fn bitor(self, rhs: Self) -> Self::Output {
-        let mut storage: [usize; STORAGE_SIZE] = unsafe { std::mem::MaybeUninit::uninit().assume_init() };
+        let mut storage: [usize; STORAGE_SIZE] =
+            unsafe { std::mem::MaybeUninit::uninit().assume_init() };
 
         for i in 0..STORAGE_SIZE {
             storage[i] = self.storage[i] | rhs.storage[i];
         }
 
-        Self {
-            storage
-        }
+        Self { storage }
     }
 }
 
@@ -193,15 +191,14 @@ impl<const BITS: usize, const STORAGE_SIZE: usize> BitXor for BitSet<BITS, STORA
 
     #[inline]
     fn bitxor(self, rhs: Self) -> Self::Output {
-        let mut storage: [usize; STORAGE_SIZE] = unsafe { std::mem::MaybeUninit::uninit().assume_init() };
+        let mut storage: [usize; STORAGE_SIZE] =
+            unsafe { std::mem::MaybeUninit::uninit().assume_init() };
 
         for i in 0..STORAGE_SIZE {
             storage[i] = self.storage[i] ^ rhs.storage[i];
         }
 
-        Self {
-            storage
-        }
+        Self { storage }
     }
 }
 
@@ -210,7 +207,9 @@ pub struct BitsIter<'a, const BITS: usize, const STORAGE_SIZE: usize> {
     bit: usize,
 }
 
-impl<'a, const BITS: usize, const STORAGE_SIZE: usize> Iterator for BitsIter<'a, BITS, STORAGE_SIZE> {
+impl<'a, const BITS: usize, const STORAGE_SIZE: usize> Iterator
+    for BitsIter<'a, BITS, STORAGE_SIZE>
+{
     type Item = bool;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -229,8 +228,9 @@ impl<'a, const BITS: usize, const STORAGE_SIZE: usize> Iterator for BitsIter<'a,
     }
 
     fn count(self) -> usize
-        where
-            Self: Sized, {
+    where
+        Self: Sized,
+    {
         BITS
     }
 }
@@ -246,13 +246,13 @@ mod tests {
     fn test_new_with_redundant_space_allocated1() {
         BitSet::<0, 1>::ones();
     }
-    
+
     #[test]
     #[should_panic]
     fn test_new_with_redundant_space_allocated2() {
-        BitSet::<{USIZE_BITS - 1}, 2>::ones();
+        BitSet::<{ USIZE_BITS - 1 }, 2>::ones();
     }
-    
+
     #[test]
     #[should_panic]
     fn test_new_with_redundant_space_allocated3() {
@@ -264,19 +264,28 @@ mod tests {
         assert_eq!(BitSet::<1, 1>::unused_bits(), USIZE_BITS - 1);
         assert_eq!(BitSet::<2, 1>::unused_bits(), USIZE_BITS - 2);
         assert_eq!(BitSet::<USIZE_BITS, 1>::unused_bits(), 0);
-        assert_eq!(BitSet::<{USIZE_BITS - 1}, 1>::unused_bits(), 1);
-        assert_eq!(BitSet::<{USIZE_BITS * 2}, 2>::unused_bits(), 0);
+        assert_eq!(BitSet::<{ USIZE_BITS - 1 }, 1>::unused_bits(), 1);
+        assert_eq!(BitSet::<{ USIZE_BITS * 2 }, 2>::unused_bits(), 0);
     }
 
     #[test]
     fn test_unused_mask() {
-        assert_eq!(BitSet::<1, 1>::unused_mask(),                0b1 << (USIZE_BITS - 1));
-        assert_eq!(BitSet::<2, 1>::unused_mask(),                0b11 << (USIZE_BITS - 2));
-        assert_eq!(BitSet::<3, 1>::unused_mask(),                0b111 << (USIZE_BITS - 3));
-        assert_eq!(BitSet::<USIZE_BITS, 1>::unused_mask(),       usize::MAX);
-        assert_eq!(BitSet::<{USIZE_BITS - 1}, 1>::unused_mask(), usize::MAX << 1);
-        assert_eq!(BitSet::<{USIZE_BITS + 1}, 2>::unused_mask(), 0b1 << (USIZE_BITS - 1));
-        assert_eq!(BitSet::<{USIZE_BITS + 2}, 2>::unused_mask(), 0b11 << (USIZE_BITS - 2));
+        assert_eq!(BitSet::<1, 1>::unused_mask(), 0b1 << (USIZE_BITS - 1));
+        assert_eq!(BitSet::<2, 1>::unused_mask(), 0b11 << (USIZE_BITS - 2));
+        assert_eq!(BitSet::<3, 1>::unused_mask(), 0b111 << (USIZE_BITS - 3));
+        assert_eq!(BitSet::<USIZE_BITS, 1>::unused_mask(), usize::MAX);
+        assert_eq!(
+            BitSet::<{ USIZE_BITS - 1 }, 1>::unused_mask(),
+            usize::MAX << 1
+        );
+        assert_eq!(
+            BitSet::<{ USIZE_BITS + 1 }, 2>::unused_mask(),
+            0b1 << (USIZE_BITS - 1)
+        );
+        assert_eq!(
+            BitSet::<{ USIZE_BITS + 2 }, 2>::unused_mask(),
+            0b11 << (USIZE_BITS - 2)
+        );
     }
 
     #[test]
@@ -291,5 +300,3 @@ mod tests {
         assert!(BitSet::<USIZE_BITS, 1>::zeroes().is_empty());
     }
 }
-
-
