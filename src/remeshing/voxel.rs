27dbf4735470dd9f0eba3bdaaa10fbb22dcd4f96
dima.volovyk <dima.volovyk@gmail.com>
2024-02-05 19:25:33 +0200

    docs

--- src/remeshing/voxel.rs
+++ src/remeshing/voxel.rs
@@ -4,6 +4,33 @@ use crate::{
     voxel::{mesh_to_sdf::MeshToSdf, meshing::MarchingCubesMesher},
 };
 
+///
+/// Voxel remeshing. 
+/// This algorithm convert mesh into SDF which is then used to create a new mesh using marching cubes.
+/// So average edge length of the output mesh is equal to the voxel size.
+/// 
+/// Self-intersecting and open meshes are supported. If the input mesh is open, 
+/// the output mesh may contain holes/open edges. However, small holes are usually closed. 
+/// Also, the input mesh should have more or less consistent orientation.
+/// Output mesh is guaranteed to be manifold.
+/// 
+/// For now only f32 is supported as a underlying scalar type.
+/// 
+/// ## Example
+/// ```ignore
+/// use baby_shark::{
+///     mesh::{builder, polygon_soup::data_structure::PolygonSoup},
+///     remeshing::voxel::VoxelRemesher,
+/// };
+/// use nalgebra::Vector3;
+/// 
+/// fn main() {
+///     let mesh: PolygonSoup<f32> = builder::cube(Vector3::zeros(), 1.0, 1.0, 1.0);
+///     let mut remesher = VoxelRemesher::default().with_voxel_size(0.02);
+///     let remeshed = remesher.remesh(&mesh);
+/// }
+/// ```
+/// 
 pub struct VoxelRemesher {
     mesh_to_sdf: MeshToSdf,
     marching_cubes: MarchingCubesMesher,
