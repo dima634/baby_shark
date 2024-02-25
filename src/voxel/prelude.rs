368df95de0ca0466c5bc099eda2870f5b7c375e0
dima.volovyk <dima.volovyk@gmail.com>
2024-02-25 15:09:46 +0200

    format

--- src/voxel/prelude.rs
+++ src/voxel/prelude.rs
@@ -1,4 +1,4 @@
+pub use super::mesh_to_sdf::MeshToSdf;
 pub use super::meshing::MarchingCubesMesher;
 pub use super::sdf::Sdf;
 pub use super::sdf::SdfBuilder;
-pub use super::mesh_to_sdf::MeshToSdf;
